d50a78bcf9eacd3414e31297a05636ae81e1c94d
neosunhan <neosunhan@gmail.com>
2022-04-02 16:06:10 +0800

    Add GET function

--- webdavclient.py
+++ webdavclient.py
@@ -4,6 +4,7 @@ import xml.etree.ElementTree as ET
 from urllib.parse import unquote, urlsplit
 
 class Client:
+    chunk_size = 10000
     namespace = {"": "DAV:"}
 
     def __init__(self, hostname, user=None, password=None, timeout=30):
@@ -20,8 +21,8 @@ class Client:
         response = self.session.request(method, path, **kwargs)
         return response
 
-    def propfind(self, path="", auth=None, **kwargs):
-        response = self.send_request("PROPFIND", path=path, headers={"Depth": "1"}, auth=auth, **kwargs)
+    def propfind(self, path="", auth=None):
+        response = self.send_request("PROPFIND", path=path, headers={"Depth": "1"}, auth=auth)
         root = ET.fromstring(response.content)
 
         responses = []
@@ -44,9 +45,18 @@ class Client:
 
         return responses
 
+    def get(self, remote_path, local_path, auth=None):
+        response = self.send_request("GET", remote_path, auth=auth)
+        if os.path.isdir(local_path):
+            local_path = os.path.join(local_path, os.path.basename(remote_path))
+        with open(local_path, "wb") as local_file:
+            for chunk in response.iter_content(chunk_size=self.chunk_size):
+                local_file.write(chunk)
+
 
 if __name__ == "__main__":
     client = Client("https://demo.owncloud.com/remote.php/dav/files/demo/", "demo", "demo")
-    files = client.propfind()
-    for file in files:
-        print(file)
+    # files = client.propfind()
+    # for file in files:
+    #     print(file)
+    client.get("Documents/Example.odt", "download/testdir/test.odt")
