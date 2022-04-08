58dfb68f862b2cf9eab09f054696d7c32d77829f
neosunhan <neosunhan@gmail.com>
2022-04-08 16:56:47 +0800

    Add tests for `propfind` and `put`

--- webdavclient.py
+++ webdavclient.py
@@ -3,7 +3,7 @@ import requests
 import xml.etree.ElementTree as ET
 from urllib.parse import unquote, urlsplit
 
-class Client:
+class WebDAVClient:
     chunk_size = 10000
     namespace = {"": "DAV:"}
 
@@ -23,24 +23,26 @@ class Client:
 
     def propfind(self, path="", auth=None):
         response = self.send_request("PROPFIND", path=path, headers={"Depth": "1"}, auth=auth)
+        if (response.status_code == 404):
+            return []
         root = ET.fromstring(response.content)
 
         responses = []
-        for response in root.findall("response", namespaces=Client.namespace):
+        for response in root.findall("response", namespaces=WebDAVClient.namespace):
             res = {}
-            res["Path"] = unquote(urlsplit(response.findtext("href", namespaces=Client.namespace)).path)
-            properties = response.find("propstat/prop", namespaces=Client.namespace)
-            if properties.find("resourcetype/collection", namespaces=Client.namespace) is not None:
+            res["Path"] = unquote(urlsplit(response.findtext("href", namespaces=WebDAVClient.namespace)).path)
+            properties = response.find("propstat/prop", namespaces=WebDAVClient.namespace)
+            if properties.find("resourcetype/collection", namespaces=WebDAVClient.namespace) is not None:
                 res["Resource Type"] = "Folder"
                 res["Name"] = res["Path"].split("/")[-2]
-                res["Size"] = int(properties.findtext("quota-used-bytes", namespaces=Client.namespace))
+                res["Size"] = int(properties.findtext("quota-used-bytes", namespaces=WebDAVClient.namespace))
             else:
-                res["Resource"] = "File"
+                res["Resource Type"] = "File"
                 res["Name"] = res["Path"].split("/")[-1]
-                res["Size"] = int(properties.findtext("getcontentlength", namespaces=Client.namespace))
-                res["Content Type"] = properties.findtext("getcontenttype", namespaces=Client.namespace)
-            res["Last Modified"] = properties.findtext("getlastmodified", namespaces=Client.namespace)
-            res["etag"] = properties.findtext("{DAV:}getetag", namespaces=Client.namespace)
+                res["Size"] = int(properties.findtext("getcontentlength", namespaces=WebDAVClient.namespace))
+                res["Content Type"] = properties.findtext("getcontenttype", namespaces=WebDAVClient.namespace)
+            res["Last Modified"] = properties.findtext("getlastmodified", namespaces=WebDAVClient.namespace)
+            res["etag"] = properties.findtext("{DAV:}getetag", namespaces=WebDAVClient.namespace)
             responses.append(res)
 
         return responses
@@ -52,17 +54,27 @@ class Client:
         with open(local_path, "wb") as local_file:
             for chunk in response.iter_content(chunk_size=self.chunk_size):
                 local_file.write(chunk)
+        return local_path
 
     def put(self, remote_path, local_path, auth=None):
         with open(local_path, "rb") as local_file:
             response = self.send_request("PUT", remote_path, data=local_file, auth=auth)
         return response.status_code
 
+    def delete(self, remote_path, auth=None):
+        response = self.send_request("DELETE", remote_path, auth=auth)
+        return response.status_code
+
+    def close(self):
+        self.session.close()
+
 
 if __name__ == "__main__":
-    client = Client("https://demo.owncloud.com/remote.php/dav/files/demo/", "demo", "demo")
-    # files = client.propfind()
+    client = WebDAVClient("https://demo.owncloud.com/remote.php/dav/files/demo/", "demo", "demo")
+    files = client.propfind("ownCloud Manual.pdf")
+    files = client.propfind("test.txt")
+    print(files)
     # for file in files:
     #     print(file)
     # client.get("Documents/Example.odt", "download/testdir/test.odt")
-    client.put("test.odt", "./download/test.odt")
+    # client.put("test.odt", "./download/test.odt")
